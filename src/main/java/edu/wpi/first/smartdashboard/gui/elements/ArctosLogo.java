9f0e4856ebbf2e3db538eb17b007d9512c0c7710
Tyler Tian <tylertian123@gmail.com>
2019-02-22 13:55:35 -0500

    Fix style in ArctosLogo

--- src/main/java/edu/wpi/first/smartdashboard/gui/elements/ArctosLogo.java
+++ src/main/java/edu/wpi/first/smartdashboard/gui/elements/ArctosLogo.java
@@ -6,25 +6,24 @@ import javax.imageio.ImageIO;
 
 public class ArctosLogo extends edu.wpi.first.smartdashboard.gui.elements.Image {
 
-    public static BufferedImage arctosLogo;
-    static {
-        try {
-          var imgStream = ClassLoader.getSystemClassLoader().getResourceAsStream("arctos1.PNG");
-          arctosLogo = ImageIO.read(imgStream);
-        }
-        catch(Exception e) {
-          System.out.println("I don't care");
-        }
+  public static BufferedImage arctosLogo;
+  static {
+    try {
+      var imgStream = ClassLoader.getSystemClassLoader().getResourceAsStream("arctos1.PNG");
+      arctosLogo = ImageIO.read(imgStream);
+    } catch (Exception e) {
+      System.out.println("I don't care");
     }
+  }
 
-    @Override
-    public void init() {
-        super.init();
-        image = arctosLogo;
+  @Override
+  public void init() {
+    super.init();
+    image = arctosLogo;
 
-        aspectKept.setValue(true);
-        propertyChanged(aspectKept);
-        revalidate();
-        repaint();
-    }
+    aspectKept.setValue(true);
+    propertyChanged(aspectKept);
+    revalidate();
+    repaint();
+  }
 }
